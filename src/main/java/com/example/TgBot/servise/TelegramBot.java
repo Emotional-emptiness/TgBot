4249e23553dc4da46c12b3692ff9c9ed39916183
Emotional-emptiness <moon.linexx@gmail.com>
2023-10-24 14:14:40 +0300

    ...

---
+++ src/main/java/com/example/TgBot/servise/TelegramBot.java
@@ -0,0 +1,54 @@
+package com.example.TgBot.servise;
+
+import com.example.TgBot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot{
+    final BotConfig config;
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                StartCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+            }
+        }
+    }
+    private void StartCommandReceived(long chatID, String name) {
+        String answer = "u name -> : " + name;
+        SendMessageMethod(chatID, answer);
+    }
+    private void SendMessageMethod(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+}
